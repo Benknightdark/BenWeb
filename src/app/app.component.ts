75e0097c31fe931d5347415bdd319560ded5a687
Benknightdark <moon791017@gmail.com>
2017-08-16 21:26:38 +0800

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  percent: number;
+  options: any;
+  constructor() {
+    this.percent = 80;
+    this.options = {
+      barColor: '#ef1e25',
+      trackColor: '#f9f9f9',
+      scaleColor: '#dfe0e0',
+      scaleLength: 5,
+      lineCap: 'round',
+      lineWidth: 3,
+      size: 110,
+      rotate: 0,
+      animate: {
+        duration: 1000,
+        enabled: true
+      }
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { EasyPieChartModule } from 'ng2modules-easypiechart';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EasyPieChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
