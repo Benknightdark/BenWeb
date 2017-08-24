ac2fdb2599cde8d861a3b8b1916d5bc18a0796c0
Benknightdark <moon791017@gmail.com>
2017-08-24 11:29:14 +0800

    add firebase package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: ' AIzaSyCpfQQd7HYcZjEd5CmUmT0PAs7qJIBM0oQ',
+    authDomain: 'benweb-c6aee.firebaseapp.com',
+    databaseURL: 'https://benweb-c6aee.firebaseio.com',
+    projectId: 'benweb-c6aee',
+    storageBucket: 'benweb-c6aee.appspot.com',
+    messagingSenderId: '328555353303'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: ' AIzaSyCpfQQd7HYcZjEd5CmUmT0PAs7qJIBM0oQ',
+    authDomain: 'benweb-c6aee.firebaseapp.com',
+    databaseURL: 'https://benweb-c6aee.firebaseio.com',
+    projectId: 'benweb-c6aee',
+    storageBucket: 'benweb-c6aee.appspot.com',
+    messagingSenderId: '328555353303'
+  }
 };
