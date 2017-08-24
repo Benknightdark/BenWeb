3b09b66ebf7f481e3d2e515a85182a5b11ed1b56
Benknightdark <moon791017@gmail.com>
2017-08-24 15:18:21 +0800

    add adminlayout content

--- src/app/admin/adminlayout/adminlayout.component.ts
+++ src/app/admin/adminlayout/adminlayout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-adminlayout',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminlayout.component.css']
 })
 export class AdminlayoutComponent implements OnInit {
+  ScreenWidth = 0;
+  constructor(private account: LoginService, private router: Router) { }
 
-  constructor() { }
+
 
   ngOnInit() {
+    this.ScreenWidth = document.documentElement.clientWidth;
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav({
+      closeOnClick: false,
+      draggable: true
+    });
+
+    Observable.fromEvent(window, 'resize')
+      .map(() => {
+        return document.documentElement.clientWidth;
+      })
+      .subscribe(data => {
+        //   console.log(data)
+
+        this.ScreenWidth = data;
+        // this.isCloseOnClick(data);
+      });
+
+    Observable.fromEvent(window, 'load')
+      .map(() => {
+        return document.documentElement.clientWidth;
+      })
+      .subscribe(data => {
+        //   console.log(data)
+        this.ScreenWidth = data;
+        // this.isCloseOnClick(data);
+
+
+      });
   }
 
+  isCloseOnClick(data) {
+    $('.collapsible').collapsible();
+    if (data < 992) {
+      $('.button-collapse').sideNav({
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      });
+    } else {
+      $('.button-collapse').sideNav({
+        closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      });
+    }
+  }
+
+  onClick() {
+    this.account.Logout();
+  }
 }
+
+
